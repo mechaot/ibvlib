182c57539dda91299340c24c6c9128f3a4126d1e
Andreas Pösch <andreas.poesch@apvy>
2020-10-29 20:11:02 +0100

    added HttpCamera with visionmaker example

---
+++ HttpCamera.py
@@ -0,0 +1,48 @@
+'''
+    Simple USB-Camera class
+'''
+
+import requests
+from urllib.parse import quote
+from imageio import imread
+
+class HttpCamera():
+    '''
+        get images from a webcam or anything with a generic camera driver
+    '''
+    def __init__(self, uri, ssl_verify=True):
+        '''
+            :param cam_no: opencvs camera index
+        '''
+        self._uri = uri
+        self._verify = ssl_verify
+        self._options = {"width": 800, "height": 600}
+
+    def set_parameter(self, **kwargs):
+        '''
+            dummy function for API compatibility with *real* cameras
+        '''
+        self._options.update(kwargs)
+
+    def get_parameter(self, **kwargs):
+        '''
+            dummy function for compatibility
+        '''
+        return None
+
+    def grab(self):
+        '''
+            get next image
+
+            :returns: next image in stack or None if no image available
+        '''
+        query_string = "&".join([quote(f"{k}={v}") for k, v in self._options.items()])
+        url = self._uri + "?" + query_string
+
+        res = requests.get(url)
+        if res.status_code == 200:
+            img = imread(res.content)
+            return img
+        else:
+            print(res.status_code, res.text)
+            return None
--- test_camera.py
+++ test_camera.py
@@ -9,10 +9,12 @@ from time import sleep
 from FileCamera import FileCamera
 from UsbCamera import UsbCamera
 from HttpStreamCamera import HttpStreamCamera
+from HttpCamera import HttpCamera
 
-cam = FileCamera("./data", cycle_images=100)
+# cam = FileCamera("./data", cycle_images=100)
 # cam = UsbCamera(0)
-#äcam = HttpStreamCamera("http://192.168.10.182:8080/video?.mjpeg")
+# cam = HttpStreamCamera("http://192.168.10.182:8080/video?.mjpeg")
+cam = HttpCamera("http://visionmaker:5000/cam")
 
 while True:
     img = cam.grab()

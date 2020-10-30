ae6ff3678154f2fcd05b5a51061336aa5b5cc628
Andreas Pösch <andreas.poesch@apvy>
2020-10-31 00:20:23 +0100

    ensure auto-install of requirements for pip git+https

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="ibvlib",
     version="0.1",
@@ -10,6 +13,7 @@ setuptools.setup(
     author_email="andreas.poesch@googlemail.com",
     description="Python functions for Lecture IBV",
     long_description=long_description,
+    install_requires=requirements,
     long_description_content_type='text/markdown',
     url="https://github.com/mechaot/ibvlib",
     packages=setuptools.find_packages(),

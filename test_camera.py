9396b9eec65cdcb5d751be5ba9ae992f610beeda
Andreas Pösch <andreas.poesch@apvy>
2020-11-01 21:57:34 +0100

    improved comments, added pytest to requirements (for later convenience)

--- test_camera.py
+++ test_camera.py
@@ -1,10 +1,10 @@
-'''
+"""
     (c) 2020 Andreas Pösch (andreas.poesch@googlemail.com)
     MIT License
 
     Make instances of one of the camera classes and grab images until pressing Esc or Space
     Pressing "s" will save current image frame
-'''
+"""
 # do not be too harsh on test scripts
 # pylint: disable=invalid-name, unused-import
 
@@ -19,20 +19,20 @@ from ibvlib.camera.UsbCamera import UsbCamera
 
 # cam = FileCamera("./data", cycle_images=100)
 cam = UsbCamera(0)
-# cam = HttpStreamCamera("http://192.168.10.182:8080/video?.mjpeg", fast_mode=False)
+# cam = HttpStreamCamera("http://192.168.10.130:8080/video?.mjpeg", fast_mode=False)
 # # This works for the Android App "IP Webcam" https://play.google.com/store/apps/details?id=com.pas.webcam
 # cam = HttpCamera("http://visionmaker:5000/cam")
 
 while True:
-    img = cam.grab()
+    img = cam.grab() # F12
     if img is None:
         print("Got no image")
         break
 
-    if img.ndim == 3: # color images need conversion
-        cv2.imshow('frame', img)  # RGB -> BGR for opencv imshow
-        # cv2.imshow('frame', img[:, :, ::-1])  # RGB -> BGR for opencv imshow
-    else: # gray images are OK
+    if img.ndim == 3: # color images dimensions=(height, width, channels) need conversion
+        cv2.imshow('frame', img)  # just display image
+        # cv2.imshow('frame', img[:, :, ::-1])  # RGB -> BGR for opencv imshow; reverse order in 3rd matrix dimension
+    else: # gray images dimensions=(height, width) are non-problematic
         cv2.imshow('frame', img)
 
     # read keyboard input, required to make the image buffer show
